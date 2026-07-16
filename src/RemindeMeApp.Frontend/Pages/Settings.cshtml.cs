3642c138f68d981d2c7c5ffd2a5c3b30ba8a26f4
luisgustavoreimberg <luisgustavoreimberg@gmail.com>
2026-07-16 00:31:11 -0300

    feat(ui): implement sidebar navigation and remove custom window controls

---
+++ src/RemindeMeApp.Frontend/Pages/Settings.cshtml.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RemindeMeApp.Frontend.Pages;
+
+public class SettingsModel : PageModel
+{
+    public void OnGet()
+    {
+    }
+}
---
+++ src/RemindeMeApp.Frontend/Pages/Tags.cshtml.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RemindeMeApp.Frontend.Pages;
+
+public class TagsModel : PageModel
+{
+    public void OnGet()
+    {
+    }
+}
